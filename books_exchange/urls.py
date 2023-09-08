7a283ca9bc472ad687d324d371186f4686e0f14f
kairat <kairat.rmk@gmail.com>
2023-09-08 15:02:59 +0600

    settings url media

--- books_exchange/urls.py
+++ books_exchange/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from exchange_app.views import *
 from exchange_app import views
@@ -43,4 +45,4 @@ urlpatterns = [
     path('swagger<str:format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-re2doc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
